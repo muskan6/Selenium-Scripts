74cb00e1692c361edfdc37b9bc1ba4372a3f763d
muskan6 <muskan99232@gmail.com>
2020-03-15 13:36:48 +0530

    commented code

--- uploadPicsOnFacebookUsingAutoIt.java
+++ uploadPicsOnFacebookUsingAutoIt.java
@@ -25,47 +25,46 @@ public class uploadPicsOnFacebookUsingAutoIt extends logindetails {
 		WebDriverWait wait = new WebDriverWait(driver, 30);
 		wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Photo/Video"))).click();
 		// Run the AutoIt Script
-		//Auto It script running -> see readme
-		Thread.sleep(8000);
+		// Auto It script running -> see readme
+		Thread.sleep(8000); // You need to add this sleep to close any popups that appear on the screen.
+		// This command is used to run a file and compile it using command line -> here
+		// i have created a scite file named "upload_files.au3" in the respective path
+		// folders as shown.
+		// After you install the AutoIt software into your system, the code is compiled
+		// using this path in your system(path to aut2exe.exe)
+		// -> C:\\Program Files (x86)\\AutoIt3\\SciTE\\..\\aut2exe\\aut2exe.exe
 		String command = "C:\\Program Files (x86)\\AutoIt3\\SciTE\\..\\aut2exe\\aut2exe.exe /in C:\\Users\\MUSKAAN\\Desktop\\selenium_upload\\upload_files.au3";
+		// exec(command) function helps in executing the command line command at runtime, which helps in compiling the .au3 script file.
 		Runtime.getRuntime().exec(command);
-		Thread.sleep(2000);
+		Thread.sleep(2000); // this sleep is temporary and avoidable, but you need some explicit wait to have the compiled upload_files.exe file in your system
+		// executing the AutoIt file executes the file which adds all the paths of the image files to the non-browser file name input box.
 		Runtime.getRuntime().exec("C:\\Users\\MUSKAAN\\Desktop\\selenium_upload\\upload_files.exe");
+		//If you have a file that tells you who you need to tag on with the post, read the file and add all its names into the names list.
 		ArrayList<String> names = new ArrayList<>();
 		try {
-		      File myObj = new File("C:\\Users\\MUSKAAN\\Desktop\\selenium_upload\\tagnames.txt");
-		      Scanner myReader = new Scanner(myObj);
-		      while (myReader.hasNextLine()) {
-		        String data = myReader.nextLine();
-		        names.add(data);
-		      }
-		      myReader.close();
-		    } catch (FileNotFoundException e) {
-		      System.out.println("Couldnt find file.");
-		      e.printStackTrace();
-		    }
-		Thread.sleep(5000);
+			//Path of file i created having the names of people to be tagged -> tagnames.txt
+			File myObj = new File("C:\\Users\\MUSKAAN\\Desktop\\selenium_upload\\tagnames.txt");
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				names.add(data);
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Couldnt find file.");
+			e.printStackTrace();
+		}
+		Thread.sleep(5000); // this sleep is editable/avoidable depending on how long your browser takes to load all the images.
 		for (int i = 0; i < names.size(); i++) {
 			WebElement tagbox = null;
 			tagbox = driver.findElement(By.xpath(
 					"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[1]/div[2]/div/div[3]/div/div/div[2]/div[1]/div/div/div/div[2]/div/div[1]/div[2]/div[1]/div/table/tbody/tr/td[2]/div/div/span[2]/label/input"));
 			tagbox.sendKeys(names.get(i));
-			Thread.sleep(1000);
+			Thread.sleep(1000); //only for better visibility of how the tagging is happening
 			tagbox.sendKeys(Keys.ENTER);
 		}
-		Thread.sleep(10000);
-		// By.cssSelector("div[role='textbox']")
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button/span[.=\"Post\"]"))).click();
+		Thread.sleep(2000); //for visibility
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button/span[.=\"Post\"]"))).click(); // post the post!!
 	}
 }
 
-// class name for Tag friends - _m_1 _1pn-
-/*
- * try { wait = new WebDriverWait(driver, 300); boolean found =
- * wait.until(ExpectedConditions.elementToBeSelected(By.xpath(
- * "/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[1]/div[2]/div/div[3]/div/div/div[2]/div[1]/div/div/div/div[2]/div/div[1]/div[2]/div[1]/div/table/tbody/tr/td[2]/div/div/span[2]/label/input"
- * ))); if(found) { tagbox = driver.findElement(By.xpath(
- * "/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[1]/div[2]/div/div[3]/div/div/div[2]/div[1]/div/div/div/div[2]/div/div[1]/div[2]/div[1]/div/table/tbody/tr/td[2]/div/div/span[2]/label/input"
- * )); tagbox.sendKeys(name); Thread.sleep(1000); tagbox.sendKeys(Keys.ENTER); }
- * }finally{ System.out.println("not found box"); }
- */
